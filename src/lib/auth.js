90e269eed7614dd7176f960d2cb296567363a6f1
probillal <smbillalhossain273@gmail.com>
2026-07-11 18:52:03 +0600

    Create a mongodb account

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,8 +2,8 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.BETTER_AUTH_SECRET);
-const db = client.db();
+const client = new MongoClient(process.env.AUTH_DB_URI);
+const db = client.db("better-auth-db");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
