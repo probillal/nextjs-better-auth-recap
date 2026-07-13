444bf16b5743bbd65a622c8885c0d230884519d6
probillal <smbillalhossain273@gmail.com>
2026-07-13 22:59:40 +0600

    Create signin page and login successfully

---
+++ src/app/auth/signin/page.jsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { Check } from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+
+const SignInPage = () => {
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const userData = Object.fromEntries(formData.entries());
+    console.log("Form data:", userData);
+    const { error, data } = await authClient.signIn.email({
+      email: userData.email,
+      password: userData.password,
+      remember: true,
+      callbackURL: "/",
+    });
+    console.log("Sign in response:", { data, error });
+    if (error) {
+      alert(`Error signing in: ${error.message}`);
+    }
+    if (data) {
+      alert("Sign in successful!");
+    }
+  };
+  return (
+    <div>
+      <h2>Please sign in</h2>
+      <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input name="email" placeholder="Enter your email" />
+          <FieldError />
+        </TextField>
+        <TextField
+          isRequired
+          minLength={8}
+          name="password"
+          type="password"
+          validate={(value) => {
+            if (value.length < 8) {
+              return "Password must be at least 8 characters";
+            }
+            if (!/[A-Z]/.test(value)) {
+              return "Password must contain at least one uppercase letter";
+            }
+            if (!/[0-9]/.test(value)) {
+              return "Password must contain at least one number";
+            }
+            return null;
+          }}
+        >
+          <Label>Password</Label>
+          <Input name="password" placeholder="Enter your password" />
+          <Description>
+            Must be at least 8 characters with 1 uppercase and 1 number
+          </Description>
+          <FieldError />
+        </TextField>
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default SignInPage;
